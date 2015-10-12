d97b659abe92da352f81f81a28dd4fa803b0e532
Niklas Ek <nikek@spotify.com>
2015-10-12 21:42:42 +0200

    [init] Working start with server-side rendering and routing

---
+++ index.js
@@ -0,0 +1,54 @@
+// IMPORT
+var fs = require('fs'),
+    http = require('http'),
+    router = require('grapnel-server'),
+    statics = require('node-static'),
+    riot = require('riot'),
+    tag = require('./public/home.js');
+
+
+// CONSTRUCT
+var route = router.start();
+var staticFiles = new statics.Server('./public');
+
+
+// CONSTANTS
+var indexHtml = fs.readFileSync('./index.html', 'utf-8');   // LAYOUT
+var mainTag = 'app';
+var defaultState = { dynamic: 'AWESOMENESSSSSSS' };
+
+
+// Inject riot rendered view into index.html
+var injectRiotView = function(state) {
+  console.log(state);
+
+  // Render riot root tag with app state
+  var view = riot.render(mainTag, state);
+  var regex = new RegExp('<' + mainTag + '(/>|>.*<\/' + mainTag + '>)'); // match <app/> or <app></app>
+
+  return indexHtml.replace(regex, view);
+};
+
+
+// RIOT RESPONSE
+var riotify = function(req, res, next) {
+  res.writeHeader(200, {"Content-Type": "text/html"});
+  res.end(injectRiotView(Object.assign({}, defaultState, {dynamic: req.url})));
+};
+
+
+// ROUTES
+router.get('/', riotify);
+router.get('/hej', riotify);
+
+
+// START SERVER
+http.createServer(function(request, response) {
+  // handle static files
+  request.addListener('end', function () {
+    staticFiles.serve(request, response);
+  }).resume();
+
+  // route to router
+  route(request, response);
+}).listen(3000);
---
+++ public/home.js
@@ -0,0 +1,5 @@
+riot.tag('app', '<h1>Hejsan Home tag {dynamic}</h1>', function(opts) {
+
+  this.dynamic = opts.dynamic || 'oklart'
+
+});
